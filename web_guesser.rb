9e6e31d63d066854acddbb47eaf7a4b79d5ad770
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2019-04-15 14:01:41 -0700

    limits amount of guesses, can play more than one game

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,24 +1,42 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-set :random_number, rand(100)
+$random_number = rand(100)
 
-def check_guess(guess, random_number)
-  if guess > random_number + 5
-    "Way too high!"
-  elsif guess < random_number - 5
-    "Way too low!" 
-  elsif guess > random_number
-    "Too high!"
-  elsif guess < random_number
-    "Too low!"
-  else
-    "You got it right!"
+class Game
+  @@attempts = 5
+  
+  def check_guess(guess, random_number)
+    @@attempts -= 1
+  
+    if @@attempts >= 0
+      if guess > $random_number + 5
+        "Way too high!" 
+      elsif guess < $random_number - 5
+        "Way too low!"
+      elsif guess > $random_number
+        "Too high!"
+      elsif guess < $random_number
+        "Too low!"
+      else
+        new_game
+        "You got it right! Guess again?"
+      end
+    else
+      new_game  
+      "GAME OVER! Guess again?"
+    end
   end
-end
 
+  def new_game
+    @@attempts = 5
+    $random_number = rand(100)
+  end
+
+end
 get '/' do
   guess = params["guess"].to_i
-  message = check_guess(guess, settings.random_number)
-  erb :index, :locals => {:number => settings.random_number, :message => message, :guess => guess }
+  play = Game.new
+  message = play.check_guess(guess, $random_number)
+  erb :index, :locals => {:number => $random_number, :message => message, :guess => guess, :attempts => Game.class_variable_get(:@@attempts)}
 end
